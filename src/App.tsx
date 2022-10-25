b511ae7519bd54a6add501c4a7a50f266d0e4899
Douglas Vinicio <douglasvinicio@Douglass-MacBook-Pro.local>
2022-10-25 12:21:39 -0400

    Completed

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+
+import { MovieCard } from './components/MovieCard';
+
+import { SideBar } from './components/SideBar';
+import { Content } from './components/Content';
+
+import { api } from './services/api';
+
+import './styles/global.scss';
+
+import './styles/sidebar.scss';
+import './styles/content.scss';
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+
+
+export function App() {
+  const [selectedGenreId, setSelectedGenreId] = useState(1);
+  const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  useEffect(() => {    
+    api.get<GenreResponseProps>(`genres/${selectedGenreId}`).then(response => {
+      setSelectedGenre(response.data);
+    })
+  }, [selectedGenreId]);
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <SideBar handleClickButton={handleClickButton} selectedGenreId={selectedGenreId} /> 
+      <Content selectedGenre={selectedGenre} selectedGenreId={selectedGenreId} />
+    </div>
+  )
+}
